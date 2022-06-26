a830cf85a8f215df058cbb5ac6cdcde30fa55e6d
TaeJeong <xowjd1013@naver.com>
2022-06-26 22:50:35 +0900

    rr

---
+++ HowToBeRich-master/src/main/java/com/example/HowToBeRich/HowToBeRichApplication.java
@@ -0,0 +1,13 @@
+package com.example.HowToBeRich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToBeRichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToBeRichApplication.class, args);
+	}
+
+}
